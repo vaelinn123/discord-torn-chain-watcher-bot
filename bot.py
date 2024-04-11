93e275f6a55b4029685c4ae67dfb41b04e6334cb
vaelinn123 <vaelinn@gmail.com>
2024-04-10 22:43:06 -0700

    Cleaning up chain-info message and removing some print lines.

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ class Bot(commands.Bot):
 
 bot = Bot()
 rotation = []
+timeToTimeout = 0
 
 @bot.command(name="rotation-clear", description="Clears the rotation.")
 async def clear_rotation(ctx):
@@ -34,12 +35,9 @@ async def clear_rotation(ctx):
 @bot.command(name="rotation-info", description="Prints chain information.")
 async def rotation_info(ctx):
     print("rotation : ", rotation)
-    # rotation_usernames = ', '.join([member.name for member in rotation])
     rotation_usernames = [f"{i+1}. {member.name}" for i, member in enumerate(rotation)]
     rotation_str = '\n'.join(rotation_usernames)
-    await ctx.send(content=f'Current rotation:\n{rotation_str}')
-    # await ctx.send(f'Current rotation: {rotation_usernames}')
-    # await ctx.send(content=f'Current rotation: {rotation_usernames}')
+    await ctx.send(content=f'```Current rotation:\n{rotation_str}```')
 
 @bot.command(name="chain-timer", description="Prints chain information.")
 async def get_faction_chain_timer(ctx):
@@ -49,14 +47,15 @@ async def get_faction_chain_timer(ctx):
     response = requests.get(url).json()
     timeout = response['chain']['timeout']
     current = response['chain']['current']
-    print("Response : ", response)
+    check_and_rotate_if_time_to_timeout_decreased(timeout)
+    global timeToTimeout
+    timeToTimeout = timeout
     await ctx.send(content=f'Faction chain count: {current}, time remaining: {timeout} seconds.')
 
 @bot.command(name="rotation-add", description="Adds a user to the rotation.")
 async def handle_rotation_add(ctx):
     try:
         user = await get_and_validate_member(ctx)
-        print("User : ", user)
         await add_user_to_rotation(user, ctx)
         await rotation_info(ctx)
     except Exception as error:
@@ -113,5 +112,11 @@ async def get_and_validate_member(ctx):
 
     await ctx.send(f"{username} is not in the room.")
     raise Exception(f"User {username} not found in the room")
+
+def check_and_rotate_if_time_to_timeout_decreased(timeout):
+    global timeToTimeout
+    if timeout < timeToTimeout:
+        timeToTimeout = timeout
+        rotation.append(rotation.pop(0))
 
 bot.run(config["BOT_TOKEN"])
